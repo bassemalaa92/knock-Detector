04ad6e9c3dad679edbfc8cc761dcc1ce4d43871e
bassemalaa92 <36682878+bassemalaa92@users.noreply.github.com>
2018-02-21 02:57:33 +0200

    Update Knock_detector.c

--- Knock_detector.c
+++ Knock_detector.c
@@ -11,16 +11,16 @@
 
 void Knock_Dect_Init(void)
 {
-	Make_Pin_Ip(A);        //make PORTA pin Do  input
+	Make_Pin_Ip(A,2);        //make PORTA pin Do  input
 }
 
 unsigned char Knock_Dect_read(void)
 {
-	if(Read_Pin(D))     // when the sensor detect a gas or smoke its output will be 1
+	if(Read_Pin(A,2))     // when the sensor detect a knock, its output will be 1
 	{
      return 1;
 	}
-	else                 // the sensor give (0) as a digital output in the normal conditions (when there is no gas or smoke detected )
+	else                 // the sensor give (0) as a digital output in the normal conditions (when there is no knock detected )
 	{
 		return 0;
 	}
